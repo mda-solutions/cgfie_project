4fb3682e9bafd3ebdbfb2c96788786b177aeef26
MDA Solutions <mda.software.solutions@gmail.com>
2013-05-20 16:07:19 -0500

    Creación de  acciones de formación (cursos)

---
+++ webapp/src/Cgfie/InscriptionsBundle/Admin/CourseAdmin.php
@@ -0,0 +1,52 @@
+<?php
+
+	namespace Cgfie\InscriptionsBundle\Admin;
+
+	use Sonata\AdminBundle\Admin\Admin;
+	use Sonata\AdminBundle\Datagrid\ListMapper;
+	use Sonata\AdminBundle\Datagrid\DatagridMapper;
+	use Sonata\AdminBundle\Form\FormMapper;
+	use Sonata\AdminBundle\Validator\ErrorElement;
+
+	class CourseAdmin extends Admin
+	{
+	    protected function configureFormFields(FormMapper $formMapper)
+	    {
+	        $formMapper
+	            ->add('name', null, array('label'=> 'Nombre'))
+	            ->add('mode', 'choice', array('label' => 'Modalidad','choices' => CourseConfig::getModes()))
+	            ->add('cur', null, array('label'=> 'Clave Unica de Registro'))
+	            ->add('hours', null, array('label'=> 'Duración (Hrs)'))
+
+	        ;
+	    }
+
+	    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+	    {
+	        $datagridMapper
+	            ->add('name', null, array('label'=> 'Nombre'))
+	            ->add('mode', null, array('label'=> 'Modalidad'))
+	            ->add('cur', null, array('label'=> 'CUR'))
+
+	        ;
+	    }
+
+	    protected function configureListFields(ListMapper $listMapper)
+	    {
+	        $listMapper
+	            ->addIdentifier('cur', null, array('label'=> 'CUR'))
+	            ->add('name', null, array('label'=> 'Nombre'))
+	            ->add('mode', null, array('label'=> 'Modalidad'))
+
+	        ;
+	    }
+	    
+	}
+
+	class CourseConfig
+	{
+		static public function getModes()
+		{
+			return array('Escolarizada', 'Mixta', 'No Escolarizada');
+		}
+	}
---
+++ webapp/src/Cgfie/InscriptionsBundle/Entity/Course.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Cgfie\InscriptionsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Course
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Cgfie\InscriptionsBundle\Entity\CourseRepository")
+ */
+class Course
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=100)
+     * @Assert\NotBlank 
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mode", type="string", length=50)
+     * @Assert\NotBlank 
+     */
+    private $mode;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cur", type="string", length=100)
+     * @Assert\NotBlank 
+     */
+    private $cur;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="hours", type="integer")
+     * @Assert\NotBlank 
+     */
+    private $hours;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Course
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set mode
+     *
+     * @param string $mode
+     * @return Course
+     */
+    public function setMode($mode)
+    {
+        $this->mode = $mode;
+    
+        return $this;
+    }
+
+    /**
+     * Get mode
+     *
+     * @return string 
+     */
+    public function getMode()
+    {
+        return $this->mode;
+    }
+
+    /**
+     * Set cur
+     *
+     * @param string $cur
+     * @return Course
+     */
+    public function setCur($cur)
+    {
+        $this->cur = $cur;
+    
+        return $this;
+    }
+
+    /**
+     * Get cur
+     *
+     * @return string 
+     */
+    public function getCur()
+    {
+        return $this->cur;
+    }
+
+    /**
+     * Set hours
+     *
+     * @param integer $hours
+     * @return Course
+     */
+    public function setHours($hours)
+    {
+        $this->hours = $hours;
+    
+        return $this;
+    }
+
+    /**
+     * Get hours
+     *
+     * @return integer 
+     */
+    public function getHours()
+    {
+        return $this->hours;
+    }
+}
