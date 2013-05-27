b413f3cccb9a41a2cb8d66364d6f1d4399c5193e
MDA Solutions <mda.software.solutions@gmail.com>
2013-05-27 18:30:42 -0500

    Grid de participantes

--- webapp/src/Cgfie/InscriptionsBundle/Controller/DefaultController.php
+++ webapp/src/Cgfie/InscriptionsBundle/Controller/DefaultController.php
@@ -20,13 +20,36 @@ class DefaultController extends Controller
         $USERS         = $this->getDoctrine()->getRepository('CgfieInscriptionsBundle:CgfieUsers')->findAll();
         $ENTITTIES     = $this->getDoctrine()->getRepository('CgfieInscriptionsBundle:CgfieEntity')->findAll();
 
+        $PERCENTS = array();
+        for ($i=0; $i < 105 ; $i += 5) 
+        { 
+            array_push($PERCENTS, $i);
+        }
+
+        $AGREE = array('ACREDITÓ', 'NO ACREDITÓ');
+        $CALS  = array( 
+                        '0 (CERO)'      => 0,
+                        '1 (UNO)'       => 1,
+                        '2 (DOS)'       => 2,
+                        '3 (TRES)'      => 3,
+                        '4 (CUATRO)'    => 4,
+                        '5 (CINCO)'     => 5,
+                        '6 (SEIS)'      => 6,
+                        '7 (SIETE)'     => 7,
+                        '8 (OCHO)'      => 8,
+                        '9 (NUEVE)'     => 9,
+                        '10 (DIEZ)'     => 10,
+                        );
 
     	return $this->render('CgfieInscriptionsBundle:Default:format06.html.twig', 
                                                                                 array(
 
                                                                                     'COURSES'=> $COURSES,
                                                                                     'USERS'  => $USERS,
-                                                                                    'ENTITTIES'=> $ENTITTIES
+                                                                                    'ENTITTIES'=> $ENTITTIES,
+                                                                                    'PERCENTS'=> $PERCENTS,
+                                                                                    'AGREE'=> $AGREE,
+                                                                                    'CALS'=> $CALS
 
                                                                                     ));
     }
